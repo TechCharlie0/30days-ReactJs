cbce5160d1492e725a87dd7f62c671c1b107cd0b
isha kandel <ishachttri@gmail.com>
2024-11-15 00:02:38 +0545

    loading

---
+++ day 5/src/data.jsx
@@ -0,0 +1,39 @@
+const reviews = [
+    {
+        id: 1,
+        name: "Isha Kandel",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+    {
+        id: 2,
+        name: "Sahun Kandel",
+        job: "Computer Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/AbirPal.574a09ad7cb325853b29.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+    {
+        id: 3,
+        name: "Robert Pattinson",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/SoumyaBanerjee.2e2521d6029842435080.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+    {
+        id: 4,
+        name: "Robisah",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/SaikatMukherjee.033310703edff52d0532.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+    {
+        id: 5, 
+        name: "IshaRobert",
+        job: "Software Engineer", 
+        image: "https://aeccc.targettechnology.in/static/media/PranayGupta.f3c530b7630ba8efb2ab.jpg",
+        text: "I have had the pleasure of working with this team on several projects, and I am always excited.",
+    },
+];
+export default reviews;
+
