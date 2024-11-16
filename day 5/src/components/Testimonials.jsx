c398e3ffaeeb99338d49cdd2b738f72aad2e3e98
isha kandel <ishachttri@gmail.com>
2024-11-16 21:41:55 +0545

    done testimonials

--- day 5/src/components/Testimonials.jsx
+++ day 5/src/components/Testimonials.jsx
@@ -2,26 +2,23 @@ import React from "react";
 import Card from "./Card";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
-
 const Testimonials = (props) => {
     let reviews = props.reviews;
     return (
-        <div>
+        <div className="flex flex-col items-center justify-center gap-5">
             <Card review={reviews[0]} />
-            <div className="flex text-3xl mt-5 gap-3 text-violet-400 font-bold mx-auto">
-                <button className="cursor-pointer hover:text-violet-500 ">
+            <div className="flex text-3xl gap-3 text-violet-400 font-bold">
+                <button className="cursor-pointer hover:text-violet-500">
                     <FiChevronLeft />
                 </button>
 
-                <button className="cursor-pointer hover:text-violet-500 ">
+                <button className="cursor-pointer hover:text-violet-500">
                     <FiChevronRight />
                 </button>
             </div>
-            <div>
-                <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg">
-                    Surprise me
-                </button>
-            </div>
+            <button className="bg-violet-400 hover:bg-violet-600 transition-all duration-200 cursor-pointer px-10 py-2 rounded-md font-bold text-white text-lg mt-5">
+                Surprise me
+            </button>
         </div>
     );
 };
