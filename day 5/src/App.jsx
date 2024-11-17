bc7f147cf22d8987565d82810be2176ff9bfee66
isha kandel <ishachttri@gmail.com>
2024-11-17 22:19:48 +0545

    done testiomanls

--- day 5/src/App.jsx
+++ day 5/src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
       <div className='text-center'>
         <h1 className='text-4xl font-bold'>Our Testimonials</h1>
       </div>
-      <div className="bg-violet-400 h-[4px] w-[1/5] mt-1"></div>
+      <div className="bg-violet-400 h-[4px] w-[1/5] mt-3 mx-auto"></div>
 
       <Testimonials reviews={reviews} />
     </div>
