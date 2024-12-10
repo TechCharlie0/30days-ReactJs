3b5567f9f8855e3624d9b7a919cfe96e1ec9c979
isha kandel <ishachttri@gmail.com>
2024-12-10 22:00:35 +0545

    react project

--- day 11/src/main.jsx
+++ day 11/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from 'react';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App'; // Ensure this import path is correct
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
